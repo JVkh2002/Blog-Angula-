b759befbbb72f8152ae629591f73334ab02d8b96
JVkh2002 <victoravelino2002@gmail.com>
2023-05-09 12:22:12 -0300

    criação do conteúdo

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -16,6 +16,10 @@ export class BigCardComponent implements OnInit {
   @Input()
   cardDescription: string = ""
 
+  @Input()
+  id: string="0"
+
+
 
   constructor() { }
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "title": "Novo filme da Barbie é anunciado.",
+        "description": "Foi anunciado um novo filme da Barbia pela diretora baba",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRgzbe0QwpD_VRbezeW5Dy653FZAyxsTxmsw&usqp=CAU"
+    },
+    {
+        "id": 2,
+        "title": "Oppenheimer ganha novo trailer",
+        "description": "ola",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxUlVh-CnhWAI08-5Pz_d3Uj6_HPPwD_pmVw&usqp=CAU"
+    }
+]
