4ef68063c6b316664342e5c9f9fb40a7af5db20f
JVkh2002 <victoravelino2002@gmail.com>
2023-05-11 01:29:03 -0300

    Criação de novas nóticias e pequenas mudanças visuais

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,50 @@
 export const dataFake = [
     {
         "id": 1,
-        "title": "Novo filme da Barbie é anunciado.",
-        "description": "Foi anunciado um novo filme da Barbia pela diretora baba",
-        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRgzbe0QwpD_VRbezeW5Dy653FZAyxsTxmsw&usqp=CAU"
+        "title": "Pedro Pascal estrelará novo filme de terror do diretor de Barbarian",
+        "description": "Ator chileno de The Last of Us protagonizará Weapons, épico de horror de Zach Cregger",
+        "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/pedro_pascal_elenco_weapons__7rgj2864-1210x544.jpg"
     },
+
     {
         "id": 2,
         "title": "Oppenheimer ganha novo trailer",
         "description": "ola",
         "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxUlVh-CnhWAI08-5Pz_d3Uj6_HPPwD_pmVw&usqp=CAU"
+    },
+
+    {
+        "id": 3,
+        "title": "Disney+ perde 4 milhões de assinantes no primeiro trimestre de 2023",
+        "description": "ola",
+        "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/disney_plus__0hz2r7y-760x428.jpg"
+    },
+
+    {
+        "id": 4,
+        "title": "Filmes de Star Wars por Taika Waititi e Rian Johnson seguem nos planos",
+        "description": "ola",
+        "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/star_wars_filmes_novidades__z5jw62rp-760x428.jpg"
+    },
+
+    {
+        "id": 5,
+        "title": "Tom Hiddleston e Mark Hamill estarão em filme que adapta Stephen King",
+        "description": "Adaptação do conto A Vida de Chuck será dirigida e escrita por Mike Flanagan, de Doutor Sono e Residência Hill",
+        "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/tom_hiddleston_mark_hamill_filme_stephen_king__183k2u42-760x428.jpg"
+    },
+
+    {
+        "id": 6,
+        "title": "Miles Morales luta contra vilão Mancha em teaser de Aranhaverso 2",
+        "description": "Adaptação do conto A Vida de Chuck será dirigida e escrita por Mike Flanagan, de Doutor Sono e Residência Hill",
+        "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/homem_aranha_atraves_do_aranhaverso_miles_mancha__12u50z-760x428.jpg"
+    },
+
+    {
+        "id": 7,
+        "title": "Voz de Dutch em Red Dead revela aparição em Guardiões da Galáxia 3",
+        "description": "Adaptação do conto A Vida de Chuck será dirigida e escrita por Mike Flanagan, de Doutor Sono e Residência Hill",
+        "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2023/05/vitrine_dutch_de_red_dead_redemption_em_guardioes_da_galaxia_3_nerdbunker__28u2l09vk-760x428.jpg"
     }
 ]
